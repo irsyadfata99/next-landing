3163c20e61289ac09f0e0703c532d69f9ff159ce
Azhar Ashhafa Alkautsar <86655297+azharashhafa@users.noreply.github.com>
2024-10-30 13:37:15 +0700

    Revert "Azhar"

--- src/app/JK-Route/page.jsx
+++ src/app/JK-Route/page.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import Image from "next/image";
-import Listt from "..Components/Route";
 
 const Pages = () => {
   return (
     <div className="relative h-screen w-screen">
       <Image className="absolute top-0 left-0" src="/menubackground.png" alt="logo" width={933} height={612} />
-      <div>
-        <Listt />
-      </div>
     </div>
   );
 };
