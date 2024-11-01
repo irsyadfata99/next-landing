27fccf821d4cf653532c194c35272216fbbfad7b
MikeKN007 <michael.setiawan@kodingnext.com>
2024-11-01 09:19:54 +0700

    final push

--- src/app/Components/Sidebar.jsx
+++ src/app/Components/Sidebar.jsx
@@ -1,10 +1,77 @@
+"use client";
 import Image from "next/image";
+import React, { useState, useEffect } from "react";
+
 
 export default function Button() {
+  const [showBubbles, setShowBubbles] = useState(false);
+  const toggleMenu = () => {
+    setShowBubbles(prevstate => !prevstate);
+  };
+
+  useEffect(() => {
+    console.log(showBubbles);
+  }, [showBubbles]);
+
   return (
-    <div className="flex pt-0">
-      <h1 className="text-white text-5xl absolute flex pl-52 pt-32">Start</h1>
-      <Image src="/btn-home.png" alt="logo" width={20000} height={3600} />
+    <div className="absolute">
+      <div
+        className={`flex pt-10 textalign-center cursor-pointer transition-transform 0.3s transform hover:scale-110`}
+        onClick={toggleMenu}
+        width="auto"
+        height="auto"
+      >
+        <Image
+          className="mr-64 mt-36"
+          src="/more-(navigation).png"
+          alt="logo"
+          width={150}
+          height={360}
+        />
+      </div>
+      {showBubbles && (
+        <>
+          <div className="cursor-pointer">
+            <div
+              className="absolute pb-10"
+              style={{ bottom: 150, left: -70 }}
+            >
+              <Image
+                className="ml-40 mb-16"
+                src="/home-bubble.png"
+                alt="logo"
+                width={150}
+                height={360}
+              />
+            </div>
+            <div
+              className="absolute pt-0"
+              style={{ bottom: 100, left: -30 }}
+            >
+              <Image
+                className="ml-0 mb-20"
+                zIndex={-1}
+                src="/menu-bubble.png"
+                alt="logo"
+                width={150}
+                height={360}
+              />
+            </div>
+            <div
+              className="absolute pt-10"
+              style={{ bottom: 70, left: 0 }}
+            >
+              <Image
+                className="ml-20 mb-2"
+                src="/gallery-bubble.png"
+                alt="logo"
+                width={150}
+                height={10}
+              />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/app/Menu/page.jsx
+++ src/app/Menu/page.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import Sidebar from "../Components/Sidebar";
 
 const Menu = () => {
   return (
     <div className="relative h-screen w-screen">
-      <Image className="absolute top-0 left-0" src="/menubackground.png" alt="logo" width={933} height={612} />
+      <Image className="absolute top-0 left-0" src="/menubackground.png" alt="logo" width={933} height={612}/>
+      <div className="z-10 relative pt-60 flex flex-row justify-around">page
+          <div className="absolute mb-96" width="auto" height="auto" style={{ top: 20, left: 70 }}>
+            <Image className="mb-44 ml-72" src="/branches.png" alt="logo" width={500} height={3600}/>
+          </div>
+          <div className="absolute mb-96" width="auto" height="auto" style={{ top: 200, left: 150 }}>
+            <Image className="mb-44 ml-72" src="/modules.png" alt="logo" width={500} height={3600}/>
+          </div>
+        <div className="absolute mr-96 pr-96">
+          <Sidebar />
+        </div>
+      </div>
     </div>
   );
 };
