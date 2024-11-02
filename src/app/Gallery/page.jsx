a7d391ba7a8832b968750304a923b44cd69bf387
irsyadfata99 <irsyadfata99@gmail.com>
2024-11-03 05:16:54 +0700

    finish

--- src/app/Gallery/page.jsx
+++ src/app/Gallery/page.jsx
@@ -56,7 +56,7 @@ const Galleries = () => {
           </div>
           <div className="w-fit ml-20 pt-16 pl-10">
             <h1>Koding Next</h1>
-            <h1>All Branches</h1>
+            <h1>Galleries</h1>
           </div>
         </div>
       </div>
