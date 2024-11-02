99d75bcd4e97ff7387ccb29a3b2421a96e16ea08
irsyadfata99 <irsyadfata99@gmail.com>
2024-11-02 13:39:26 +0700

    ready for deploy

--- src/app/Components/Button.jsx
+++ src/app/Components/Button.jsx
@@ -2,9 +2,8 @@ import Image from "next/image";
 
 export default function Button() {
   return (
-    <div className="flex pt-0">
-      <h1 className="text-white text-5xl absolute flex pl-52 pt-32">Start</h1>
-      <Image src="/btn-home.png" alt="logo" width={20000} height={3600} />
+    <div className="flex items-center ml-60">
+      <Image src="/Start-Button.png" alt="logo" width={320} height={317} />
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="max-w-1920 max-h-1200 relative">
-      <video src="/Looping-Transisi.mov" autoPlay muted preload="metadata" loop className="absolute top-0 left-0 w-full h-full object-cover opacity-40 z-0"></video>
-      {/* <Image className="absolute top-0 left-0" src="/homebackground.png" alt="logo" width={933} height={612} /> */}
+      <video src="/Looping-Transisi.mov" autoPlay muted preload="metadata" loop className="absolute top-0 left-0 w-[1920px] h-[612px] object-cover opacity-40 z-0"></video>
+      <Image className="absolute top-0 left-0" src="/homebackground.png" alt="logo" width={933} height={612} />
       <div className="relative z-10 flex flex-row pt-44">
         <div className="text-black basis-1/4 pt-24 pl-52 flex flex-row">
           <div>
             <h1 className="w-44">Koding Next</h1>
             <p>Bandung & BSD</p>
           </div>
-          {/* Will look the alternative, but in the meantime just do like this */}
           <Image src="/logo.png" alt="logo" className="logo-image" width={200} height={100} />
         </div>
         <Link href={"/Menu"}>
@@ -22,7 +21,7 @@ export default function Home() {
         </Link>
       </div>
       <Link href={"/WhyChooseUs"}>
-        <Image className="absolute bottom-5 right-5" src="/Why-Button.png" alt="logo" width={100} height={100} />
+        <Image className="absolute bottom-22 right-5" src="/Why-Button.png" alt="logo" width={100} height={100} />
       </Link>
     </div>
   );
